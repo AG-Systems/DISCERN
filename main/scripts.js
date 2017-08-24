9a464faffd9ed7c130d2758fa4441c28364c4e95
Marc Cooperstein <mcooperstein@gmail.com>
2017-08-24 13:51:24 -0700

    about page changes
    about page changes

--- main/scripts.js
+++ main/scripts.js
@@ -1,7 +1,32 @@
 var app = angular.module("myApp", ["ngRoute"]);
+
+/*app.value("bios", [
+    //Harry
+    {
+        person: "Harry Blount",
+        image: "images/mark-zuckerberg.jpg"
+    },
+    //Marc
+    {
+        person: "Marc Cooperstein",
+        image: "images/brady.png"
+    },
+    //Elon
+    {
+        person: "Elon Musk",
+        image: "images/elon.jpg"
+    },
+    //Bezos
+    {
+        person: "Jeff Bezos",
+        image: "images/bezos.jpg"
+    }
+]);*/
+
 app.config(function ($routeProvider) {
     $routeProvider.when("/about", {
-            templateUrl: "about.html"
+            templateUrl: "about.html",
+            controller: "about_controller"
         })
         .when("/data", {
             templateUrl: "data.html"
@@ -21,4 +46,33 @@ app.config(function ($routeProvider) {
         .otherwise({
             templateUrl: "home.html"
         });
+});
+
+app.controller('about_controller', function ($scope) {
+    $scope.current_name = "";
+    $scope.bio = "";
+    $scope.image = "";
+    $scope.changeName = function (name) {
+        $scope.current_name = name;
+        console.log(name);
+    };
+    $scope.changebio = function (bio_info) {
+        $scope.bio = bio_info;
+    }
+    $scope.changeImg = function (img) {
+        $scope.image = img;
+    }
+    var modal = document.getElementById('myModal');
+    //var span = document.getElementsByTagName("span");
+    $(".bios").click(function () {
+        modal.style.display = "block";
+    });
+    $("span").click(function () {
+        modal.style.display = "none";
+    })
+    window.onclick = function (event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
 });

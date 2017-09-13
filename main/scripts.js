3696f5a4c1be0ecd56db96b3e7a11b420cb6c231
Auriga <area51alienware@gmail.com>
2017-09-13 17:38:14 +0000

    Trying to work on about page

--- main/scripts.js
+++ main/scripts.js
@@ -138,7 +138,98 @@ app.controller('about_controller', function ($scope) {
             ]
         }
     ];
+    /*
+    $scope.team = [
+        {
+            name: "Harry E. Blount",
+            bio: "Harry Blount, cofounded DISCERN in 2009, based in part on a study done for the national academy of science committee on “Forecasting Future Disruptive Tech” and his 20+ years on Wall Street with major investments firms such as brothers, credit suisse, first boston, donaldson, lukfin and jenette and CIBC oppenheimer. Previously, Harry was named an insituition investor all-Amercian in both infomation tech hardware and internet infrastructure and a wall street journal all-star covering the computer hardware sector. Harry also lent his expertise to the national academy of sciences, where he served as a member of the comittee for forecasting future disruptive tech and to the tech musuem of innovating in san jose where he chaired the futures ocmittee. Harry gradurate from the university of wisconsin la crosse in 1986 with a bs in finance and has been a cra charter holder since the early 1990s",
+            title: "Chairman & CEO",
+            img: "images/Team_Members_Harry.png"
+      },
+        {
+            
+            name: "Thor Hauge",
+            bio: "Thor Hauge has a unique background with years of expertise in innovation commercialization, peer-to-peer, digital currencies, mobile payments and big data. Previously, Thor was a Global Business Development Executive with Western Union Digital Ventures and worked in Early Stage Venture Investments at Nokia Ventures.",
+            title: "Head of Business Development",
+            img: "images/Team_Members_Thor.png"
+        },
+      {
+            name: "Jim Christie",
+            bio: "Tesla, SpaceX, Solar City... Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores porro amet laboriosam, quasi, illo obcaecati odit. Fuga blanditiis, magni maxime asperiores possimus, aliquam hic iure dignissimos iusto perspiciatis, dolorem enim?",
+            title: "Head of Product",
+            img: "images/Team_Members_Jim.png"          
+      },
+      {
+            name: "Raghu Madabusi",
+            bio: "Raghu Madabusi possess more than 20 years of design, development, and management experience in leading complex projects and high-caliber teams in USA, Germany and India. A Principal Technical Architect with strong business acumen and technical experience in Big Data, Cloud Computing & Traditional IT Projects. Raghu is PMP certified and holds close to dozen IT certifications. He has expertise in building complex Prototypes & Implementations in Big Data & Cloud that involve Machine Learning, Natural Language Processing, Graph Databases, Distributed Computing, Microservices, and more",
+            title: "CTO",
+            img: "images/Team_Members_Raghu.png"           
+      },
+      {
+            name: "Troy Hansen",
+            bio: "30+ years Senior-level Quality Assurance executive. Prior to joining DISCERN, Troy worked for General Dynamics where he was responsible for communications testing on the last two major U.S. satellite constellations. Troy has also served in senior engineering positions for a variety of other firms including Xerox, Honeywell and Fischer Scientific. Troy is an avid options and commodity trader specializing in gold securities.",
+            title: "Data Integrity & Enhancement",
+            img: "images/Team_Members_Troy.png"           
+      },
+      {
+            name: "Mark Herrmann",
+            bio: "Mark Herrmann has 18 years of institutional equity sales experience, including 9 as a Managing Director at Bear Stearns, covering the largest hedge funds and mutual funds on the west coast.  Over the past year, he has been working in a marketing and product development role for several alternative data companies.  Mark received an undergraduate degree in mechanical engineering from Rensselaer Polytechnic Institute and an MBA in finance from USC. He is based in San Francisco.",
+            title: "Data & Content Sales",
+            img: "images/Team_Members_Mark.png"           
+      },
+      {
+            name: "Dave Wigginton",
+            bio: "David Wigginton has demonstrated a consistent ability to provide differentiated coverage of the real estate investment trust sector both as a Senior Analyst at Macquarie Capital and in the equity research department of Merrill Lynch. Prior to commencing his career in research, David worked as a Senior Consultant in the Financial Services Advisory area at Ernst & Young. He is a graduate of the University of Notre Dame, where he earned an M.S. in Accountancy, and Brigham Young University, where he earned a B.S. in Business Management with a finance emphasis.",
+            title: "Real Estate Curation",
+            img: "images/Team_Members_Dave.png"           
+      },
+      {
+            name: "Rick Church",
+            bio: "Richard Church’s experience base includes 20+ years in equity investing in both public and private realms. His strengths revolve around strategy, financial advisory services, business valuation and global consumer/retail fundamental research. Richard’s background includes senior roles, such as Managing Director at Citi-Smith Barney; Managing Director at Shumway Capital Partners; and Principal/CIO at Quartern Capital Partners. Among Richard’s honors stand his distinction as a five-time winner of the Institutional Investor All-America Research Team award and the Wall Street Journal Best of the Street Award.",
+            title: "Consumer Retail Curation",
+            img: "images/Team_Members_Richard.png"           
+      },
+      {
+            name: "Gil Yang",
+            bio: "Jeff Bezos is his own bodyguard... Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga similique beatae neque, consectetur quasi, nisi. Sunt odio odit quibusdam repellendus in architecto libero corporis magnam tenetur. Nesciunt mollitia excepturi sequi.",
+            title: "Energy Curation",
+            img: "images/Team_Members_Gil.png"           
+      },
+      {
+            name: "Sanjay Agarwal",
+            bio: "Jeff Bezos is his own bodyguard... Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga similique beatae neque, consectetur quasi, nisi. Sunt odio odit quibusdam repellendus in architecto libero corporis magnam tenetur. Nesciunt mollitia excepturi sequi.",
+            title: "Chief Data Scientist",
+            img: "images/Team_Members_Sanjay.png"           
+      }
+     ];
+     */
     $scope.image = "";
+    $scope.bio_page = 0;
+    $scope.hidden = "";
+    /*
+    $scope.showbio = function (b) 
+    {
+        if ($(document).width() >= 1200)
+        {
+            bio_page = 4;
+        }
+        else if($(document).width() >= 993)
+        {
+            bio_page = 3;
+        }
+        else if($(document).width() >= 769)
+        {
+            bio_page = 2;
+        }
+        else
+        {
+            bio_page = 1;
+        }
+        $scope.hidden = b;
+        $("#" + (bio_page-1)).append("<div style='width: 100%;height: 200px;background-color: black; margin-top: 400px;'></div> ");
+        console.log($(document).width());
+    }
+    */
     $scope.changeName = function (name) {
         $scope.current_name = name;
         console.log(name);
@@ -166,6 +257,8 @@ app.controller('about_controller', function ($scope) {
             modal.style.display = "none";
         }
     }
+    
+    
 });
 
 

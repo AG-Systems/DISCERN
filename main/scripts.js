44856b9b8dbea9968a93b92554217fad97f42e83
Marc Cooperstein <mcooperstein@gmail.com>
2017-09-26 11:58:45 -0700

    code clean up

--- main/scripts.js
+++ main/scripts.js
@@ -1,26 +1,4 @@
 var app = angular.module("myApp", ["ngRoute"]);
-/*app.value("bios", [
-    //Harry
-    {
-        person: "Harry Blount",
-        image: "images/mark-zuckerberg.jpg"
-    },
-    //Marc
-    {
-        person: "Marc Cooperstein",
-        image: "images/brady.png"
-    },
-    //Elon
-    {
-        person: "Elon Musk",
-        image: "images/elon.jpg"
-    },
-    //Bezos
-    {
-        person: "Jeff Bezos",
-        image: "images/bezos.jpg"
-    }
-]);*/
 
 app.config(function ($routeProvider) {
     $routeProvider.when("/about", {
@@ -46,63 +24,47 @@ app.config(function ($routeProvider) {
             templateUrl: "news.html",
             controller: "news"
         })
-        /*.when("/services", {
-            templateUrl: "services.html"
-        })*/
         .otherwise({
             templateUrl: "home.html"
         });
-
 
 });
 app.controller('news', function ($scope, $sce, $http) {
     $scope.index = 0;
     $scope.posts = [
 
-
         {
             title: "DISCERN chosen as default search platform for Alpha Theory Portfolio Optimization platform ",
             date: "09/26/17",
             post_id: 0,
-            link: "./alpha_theory.pdf",
-            text: "Hella church-key listicle heirloom squid. Humblebrag woke biodiesel la croix post-ironic truffaut. Echo park thundercats pickled YOLO fingerstache. Food truck bespoke before they sold out, cornhole plaid semiotics four dollar toast irony snackwave squid tousled. 90's farm-to-table cray beard meggings cloud bread yr sriracha readymade... "
-
+            link: "./alpha_theory.pdf"
       },
         {
             title: "DISCERN chosen as one of 8 to watch in 2018 by Data Industry Research Leader Outsell  September 12, 2017 ",
-            date: "08/11/17",
+            date: "09/12/17",
             post_id: 1,
-            link: "https://outsellinc.com/signature-event-2017/#!/emergingcompanygrowthtank",
-            text: "Hella church-key listicle heirloom squid. Humblebrag woke biodiesel la croix post-ironic truffaut. Echo park thundercats pickled YOLO fingerstache. Food truck bespoke before they sold out, cornhole plaid semiotics four dollar toast irony snackwave squid tousled. 90's farm-to-table cray beard meggings cloud bread yr sriracha readymade... "
-
+            link: "https://outsellinc.com/signature-event-2017/#!/emergingcompanygrowthtank"
       },
 
         /*{
             title: "DISCERN to present at Battlefin West Coast on October 26 in Palo Alto",
             date: "08/01/17",
             post_id: 2,
-            link: "https://www.google.com/",
-            text: "Tbh heirloom wayfarers everyday carry distillery, shoreditch pickled glossier bespoke occupy. 3 wolf moon edison bulb portland, green juice cred raw denim adaptogen tattooed. Yuccie chillwave cardigan tattooed poke waistcoat affogato cred cloud bread trust fund pok pok semiotics typewriter literally brunch. Leggings celiac hashtag, freegan bicycle rights iPhone occupy farm-to-table copper mug tbh church-key cred. Tacos lo-fi YOLO poke. Chicharrones 90's lumbersexual, mlkshk small batch adaptogen franzen ennui four dollar toast kogi. Gentrify craft beer bitters, slow-carb cronut cred farm-to-table intelligentsia. Ramps raclette pok pok bespoke leggings tilde you probably haven't heard of them franzen lo-fi. Williamsburg jean shorts air plant, green juice YOLO knausgaard try-hard four loko helvetica brunch food truck gluten-free hell of truffaut..."
+            link: "https://www.google.com/"
 
       },*/
         {
             title: "DISCERN chosen as default search platform for Battlefin",
             date: "06/14/17",
             post_id: 2,
-            link: "http://www.battlefin.com/press-release-discern",
-            text: "Hella church-key listicle heirloom squid. Humblebrag woke biodiesel la croix post-ironic truffaut. Echo park thundercats pickled YOLO fingerstache. Food truck bespoke before they sold out, cornhole plaid semiotics four dollar toast irony snackwave squid tousled. 90's farm-to-table cray beard meggings cloud bread yr sriracha readymade... "
-
+            link: "http://www.battlefin.com/press-release-discern"
       },
         {
             title: "Is Your Data Ready?",
             date: "05/25/17",
             post_id: 3,
-            link: "./linkedin.html",
-            text: "Hella church-key listicle heirloom squid. Humblebrag woke biodiesel la croix post-ironic truffaut. Echo park thundercats pickled YOLO fingerstache. Food truck bespoke before they sold out, cornhole plaid semiotics four dollar toast irony snackwave squid tousled. 90's farm-to-table cray beard meggings cloud bread yr sriracha readymade... "
-
+            link: "./linkedin.html"
       }
-
-
     ]
 
     $scope.events = [
@@ -110,56 +72,47 @@ app.controller('news', function ($scope, $sce, $http) {
             title: "DISCERN to present at Outsell’s Emerging Company Growth Tank Conference",
             date: "10/4/17",
             post_id: 0,
-            link: "https://outsellinc.com/signature-event-2017/#!/emergingcompanygrowthtank",
-            text: "Discern Group Inc., a San Francisco-based startup that specializes in finding what it calls 'weak signals' that could affect financial markets, used public data to determine that a well drilled by an Oklahoma-based energy company one day last November performed better than 95% of the company's wells in the region, was an outlier and therefore deserved a closer look..."
-
+            //link: "https://blog.outsellinc.com/eight-nominees-for-outsells-growth-tank-c252182628af"
+            link: "https://outsellinc.com/signature-event-2017/#!/emergingcompanygrowthtank"
         },
         {
             title: "DISCERN to present at Battlefin West Coast",
             date: "10/26/17",
             post_id: 1,
-            link: "http://www.battlefin.com/press-release-discern",
-            text: "'Customers desire to spend more time on analysis and less time on mundane activities such as data aggregation, organization and presentation” says DISCERN CEO Harry Blount.  'With the addition of SNL, we can now provide real estate investors with all of the supply, demand, inventory valuation data they need, in one-location, to create and sustain a competitive information advantage...'"
+            link: "http://www.battlefin.com/press-release-discern"
       }
-
         ]
     $scope.current_tab = $scope.posts;
-    /*$scope.change_post = function (index) {
+    $scope.change_post = function (index) {
         $scope.index = index
-        $scope.current_story = $scope.posts[$scope.index]
+        if ($scope.current_tab == $scope.posts) {
+            $scope.current_story = $scope.posts[$scope.index]
+            $scope.trustSrc = function (src) {
+                return $sce.trustAsResourceUrl(src);
+            }
+        } else {
+            $scope.current_story = $scope.events[$scope.index]
+            $scope.trustSrc = function (src) {
+                return $sce.trustAsResourceUrl(src);
+            }
+        }
+
         console.log($scope.current_story);
-    };*/
+    };
     /*$scope.current_story = $scope.posts[$scope.index]
     $scope.trustSrc = function (src) {
         return $sce.trustAsResourceUrl(src);
     }*/
     $scope.show_news = function () {
-        /*if ($scope.current_tab == $scope.posts) {
-            $scope.current_tab = $scope.events;
-        } else {*/
         $scope.current_tab = $scope.posts;
         $scope.index = 0;
         $scope.current_story = $scope.posts[$scope.index];
         console.log($scope.current_story);
         $scope.trustSrc = function (src) {
-                return $sce.trustAsResourceUrl(src);
-            }
-            //}
-
-        $('#news_btn').css({
-            'background-color': '#ececec',
-            'color': 'black',
-        });
-
-        $('#events_btn').css({
-            'background-color': '#474747',
-            'color': 'white',
-        });
+            return $sce.trustAsResourceUrl(src);
+        }
     }
     $scope.show_events = function () {
-        /*if ($scope.current_tab == $scope.posts) {
-            $scope.current_tab = $scope.events;
-        } else {*/
         $scope.current_tab = $scope.events;
         $scope.index = 0;
         $scope.current_story = $scope.events[$scope.index];
@@ -167,16 +120,15 @@ app.controller('news', function ($scope, $sce, $http) {
         $scope.trustSrc = function (src) {
                 return $sce.trustAsResourceUrl(src);
             }
-            //}
-        $('#events_btn').css({
-            'background-color': '#ececec',
-            'color': 'black',
-        });
+            /*$('#events_btn').css({
+                'background-color': '#ececec',
+                'color': 'black',
+            });
 
-        $('#news_btn').css({
-            'background-color': '#474747',
-            'color': 'white',
-        });
+            $('#news_btn').css({
+                'background-color': '#474747',
+                'color': 'white',
+            });*/
     }
 });
 
@@ -387,71 +339,7 @@ app.controller('about_controller', function ($scope) {
             $("#" + (bio_page - 1)).after("<div class='col-xs-12'  id='bioShown'><div class='biotext'>" + bio_input + "</div></div>");
         }
     };
-    /*
-    $scope.team = [
-        {
-            name: "Harry E. Blount",
-            bio: "Harry Blount, cofounded DISCERN in 2009, based in part on a study done for the national academy of science committee on “Forecasting Future Disruptive Tech” and his 20+ years on Wall Street with major investments firms such as brothers, credit suisse, first boston, donaldson, lukfin and jenette and CIBC oppenheimer. Previously, Harry was named an insituition investor all-Amercian in both infomation tech hardware and internet infrastructure and a wall street journal all-star covering the computer hardware sector. Harry also lent his expertise to the national academy of sciences, where he served as a member of the comittee for forecasting future disruptive tech and to the tech musuem of innovating in san jose where he chaired the futures ocmittee. Harry gradurate from the university of wisconsin la crosse in 1986 with a bs in finance and has been a cra charter holder since the early 1990s",
-            title: "Chairman & CEO",
-            img: "images/Team_Members_Harry.png"
-      },
-        {
 
-            name: "Thor Hauge",
-            bio: "Thor Hauge has a unique background with years of expertise in innovation commercialization, peer-to-peer, digital currencies, mobile payments and big data. Previously, Thor was a Global Business Development Executive with Western Union Digital Ventures and worked in Early Stage Venture Investments at Nokia Ventures.",
-            title: "Head of Business Development",
-            img: "images/Team_Members_Thor.png"
-        },
-      {
-            name: "Jim Christie",
-            bio: "Tesla, SpaceX, Solar City... Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores porro amet laboriosam, quasi, illo obcaecati odit. Fuga blanditiis, magni maxime asperiores possimus, aliquam hic iure dignissimos iusto perspiciatis, dolorem enim?",
-            title: "Head of Product",
-            img: "images/Team_Members_Jim.png"
-      },
-      {
-            name: "Raghu Madabusi",
-            bio: "Raghu Madabusi possess more than 20 years of design, development, and management experience in leading complex projects and high-caliber teams in USA, Germany and India. A Principal Technical Architect with strong business acumen and technical experience in Big Data, Cloud Computing & Traditional IT Projects. Raghu is PMP certified and holds close to dozen IT certifications. He has expertise in building complex Prototypes & Implementations in Big Data & Cloud that involve Machine Learning, Natural Language Processing, Graph Databases, Distributed Computing, Microservices, and more",
-            title: "CTO",
-            img: "images/Team_Members_Raghu.png"
-      },
-      {
-            name: "Troy Hansen",
-            bio: "30+ years Senior-level Quality Assurance executive. Prior to joining DISCERN, Troy worked for General Dynamics where he was responsible for communications testing on the last two major U.S. satellite constellations. Troy has also served in senior engineering positions for a variety of other firms including Xerox, Honeywell and Fischer Scientific. Troy is an avid options and commodity trader specializing in gold securities.",
-            title: "Data Integrity & Enhancement",
-            img: "images/Team_Members_Troy.png"
-      },
-      {
-            name: "Mark Herrmann",
-            bio: "Mark Herrmann has 18 years of institutional equity sales experience, including 9 as a Managing Director at Bear Stearns, covering the largest hedge funds and mutual funds on the west coast.  Over the past year, he has been working in a marketing and product development role for several alternative data companies.  Mark received an undergraduate degree in mechanical engineering from Rensselaer Polytechnic Institute and an MBA in finance from USC. He is based in San Francisco.",
-            title: "Data & Content Sales",
-            img: "images/Team_Members_Mark.png"
-      },
-      {
-            name: "Dave Wigginton",
-            bio: "David Wigginton has demonstrated a consistent ability to provide differentiated coverage of the real estate investment trust sector both as a Senior Analyst at Macquarie Capital and in the equity research department of Merrill Lynch. Prior to commencing his career in research, David worked as a Senior Consultant in the Financial Services Advisory area at Ernst & Young. He is a graduate of the University of Notre Dame, where he earned an M.S. in Accountancy, and Brigham Young University, where he earned a B.S. in Business Management with a finance emphasis.",
-            title: "Real Estate Curation",
-            img: "images/Team_Members_Dave.png"
-      },
-      {
-            name: "Rick Church",
-            bio: "Richard Church’s experience base includes 20+ years in equity investing in both public and private realms. His strengths revolve around strategy, financial advisory services, business valuation and global consumer/retail fundamental research. Richard’s background includes senior roles, such as Managing Director at Citi-Smith Barney; Managing Director at Shumway Capital Partners; and Principal/CIO at Quartern Capital Partners. Among Richard’s honors stand his distinction as a five-time winner of the Institutional Investor All-America Research Team award and the Wall Street Journal Best of the Street Award.",
-            title: "Consumer Retail Curation",
-            img: "images/Team_Members_Richard.png"
-      },
-      {
-            name: "Gil Yang",
-            bio: "Jeff Bezos is his own bodyguard... Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga similique beatae neque, consectetur quasi, nisi. Sunt odio odit quibusdam repellendus in architecto libero corporis magnam tenetur. Nesciunt mollitia excepturi sequi.",
-            title: "Energy Curation",
-            img: "images/Team_Members_Gil.png"
-      },
-      {
-            name: "Sanjay Agarwal",
-            bio: "Jeff Bezos is his own bodyguard... Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga similique beatae neque, consectetur quasi, nisi. Sunt odio odit quibusdam repellendus in architecto libero corporis magnam tenetur. Nesciunt mollitia excepturi sequi.",
-            title: "Chief Data Scientist",
-            img: "images/Team_Members_Sanjay.png"
-      }
-     ];
-     */
     $scope.image = "";
     $scope.bio_page = 0;
     $scope.hidden = "";
@@ -506,7 +394,6 @@ app.controller('about_controller', function ($scope) {
             modal.style.display = "none";
         }
     }
-
 
 });
 
